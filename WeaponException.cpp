c89a3ccdefe755dbfbf1baa94831ba7cde4f9ec2
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2021-04-07 12:16:08 +0100

    Update WeaponException.cpp

--- WeaponException.cpp
+++ WeaponException.cpp
@@ -9,6 +9,7 @@ class WeaponException : public exception {
         msg.setText("You must have a weapon equipped to attack!");
         msg.setWindowFlags(Qt::FramelessWindowHint);
         msg.setStyleSheet("background-color:gray;border-style:outset");
+        msg.move(350,300);
         msg.exec();
     }
 };
