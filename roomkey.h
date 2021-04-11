b8425d9900660943a289da5e3bc3300adae28f41
ULKellyRyan <72022785+ULKellyRyan@users.noreply.github.com>
2021-04-11 16:31:47 +0100

    Update roomkey.h
    removed comments

--- roomkey.h
+++ roomkey.h
@@ -7,9 +7,9 @@ class RoomKey: public GameItem
 {
 public:
     RoomKey();
-//    string description;
+
     RoomKey(string description, string imgPath);
-//    virtual QString itemInfo();
+
 };
 
 #endif // ROOMKEY_H
